ff00e197018a2cf846a5f749443f1379819a8de2
Jannis R <mail@jannisr.de>
2017-11-14 20:42:09 +0100

    UI setup

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict'
 
-// todo
+require('babel-polyfill')
+
+const document = require('global/document')
+const snabbdom = require('snabbdom')
+const classForSnabbdom = require('snabbdom/modules/class').default
+const attrForSnabbdom = require('snabbdom/modules/class').default
+
+const render = require('./ui')
+
+const state = {
+	// todo
+}
+
+const actions = {
+	// todo
+}
+
+const patch = snabbdom.init([classForSnabbdom, attrForSnabbdom])
+
+const container = document.body
+const rerender = () => {
+	const vTree = render(state, actions)
+	patch(container, vTree)
+}
---
+++ ui/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const h = require('snabbdom/h').default
+
+const render = (state, actions) => {
+	return h('div', {}, [])
+}
+
+module.exports = render
